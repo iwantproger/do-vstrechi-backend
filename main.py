072c9d577d915d3e3a3dd3a5f099388b7487fe01
iwantproger <borllllive@gmail.com>
2026-03-14 23:27:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -440,4 +440,5 @@ async def get_bookings(telegram_id: int, status: Optional[str] = None):
 if __name__ == "__main__":
     import uvicorn
     logger.info("Starting server...")
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.getenv("PORT", 8000))
+   uvicorn.run(app, host="0.0.0.0", port=port)
